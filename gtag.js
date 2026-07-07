983e94e8cb8bbf26edd03e49457e64fc5229ad7b
Shrangrala <155485748+Shrangrala@users.noreply.github.com>
2026-07-07 10:35:14 -0500

    Delete gtag.js

--- gtag.js
+++
@@ -1,9 +0,0 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-VF5ZRYTWJD"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-VF5ZRYTWJD');
-</script>
